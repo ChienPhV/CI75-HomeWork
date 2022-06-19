ed304fef04011305e98a0499b4ee662cc3d31ad9
ChienPhV <phvchien@gmail.com>
2022-06-20 00:07:36 +0700

    add tìm kiếm danh bạ

--- B1/B1-4.js
+++ B1/B1-4.js
@@ -1,18 +1,38 @@
-//hàm lấy giá trị và hiển thị ra màn hình
+//o	Danh bạ được sắp xếp bằng tên theo bảng chữ cái
+//o	Thêm mới danh bạ với tên và số điện thoại
+
 
 let contacts = [];
 function getContact() {
-    
+
     let Name = document.querySelector('#name').value.toUpperCase();
     let Phone = document.querySelector('#phone').value;
     if (Name && Phone) {
-        contacts.push(`<br> ${Name}, ${Phone}`);
+        contacts.push(`<br>${Name}, ${Phone}`);
     }
     contacts.sort(); // arrange text in alphabetical 
-    document.getElementById("display").innerHTML= contacts;
+    document.getElementById("display").innerHTML = contacts;
     console.log(contacts);
-    return false; 
+    return false;
 }
+
+//o	Tìm kiếm danh bạ theo tên và số điện thoại
+function search() {
+    let input, filter, a, i;
+    input = document.getElementById("search-input");
+    filter = input.value.toUpperCase();
+
+    for (i = 0; i < contacts.length; i++) {
+        a = contacts[i];
+        if (a.indexOf(filter) > -1) {
+            document.getElementById("display").innerHTML = a;
+            console.log(a.indexOf(filter))
+        }
+    }
+}
+
+
+
 
 
 
